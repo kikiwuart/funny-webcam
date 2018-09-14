cd717a89bcf8fe275846556100aac090cd5e7065
Kiki Wu <kuan-yi.wu@sjsu.edu>
2018-09-14 14:30:19 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-webcam
+#Webcam Note
+[jQuery](https://www.xarg.org/project/jquery-webcam-plugin/)
---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+
+  // Normalize the various vendor prefixed versions of getUserMedia.
+  navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia || 
+                            navigator.msGetUserMedia);
+
+}
